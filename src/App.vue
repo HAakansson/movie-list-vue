33434c62fc1b2c6a1465586c3044b4e9076ccb3e
Niklas Håkansson <n.hakansson@hotmail.com>
2021-01-26 10:37:08 +0100

    Update of movie list
    Question 1, 2, 3, 4 are done.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <template>
   <div id="app">
-    <router-view />
+    <router-view @add-new-movie="addMovieToMovies" />
   </div>
 </template>
+
+<script>
+export default {
+  data(){
+    return {
+      movies: [],
+    }
+  },
+  
+  methods: {
+    addMovieToMovies(newMovie){
+      this.movies.push(newMovie);
+      console.log("In App.vue - movies: ", this.movies)
+    }
+  }
+}
+</script>
 
 <style></style>
--- src/views/AddMovie.vue
+++ src/views/AddMovie.vue
@@ -1,83 +1,103 @@
 <template>
   <div class="add-movie">
-    <div class="title wrapper">
-      <label for="title">Title</label>
-      <input id="title" type="text" />
-    </div>
-    <div class="rating wrapper">
-      <label for="rating">Rating</label>
-      <span>1</span>
-      <input id="rating" type="range" />
-      <span>5</span>
-    </div>
-    <div class="genre wrapper">
-      <label for="genre">Genre</label>
-      <select id="genre" name="genre">
-        <option v-for="(genre, i) in genres" :key="i" value="genre">{{ genre }}</option>
-      </select>
-    </div>
-    <div class="desc wrapper">
-      <label for="desc">Description</label>
-      <textarea
-        id="description"
-        name="description"
-        cols="30"
-        rows="10"
-      ></textarea>
-    </div>
-    <div class="button wrapper">
-      <button>Clear</button>
+    <form @submit.prevent="createMovie">
+      <div class="title wrapper">
+        <label for="title">Title</label>
+        <input id="title" type="text" v-model="title" />
+      </div>
+      <div class="rating wrapper">
+        <label for="rating">Rating</label>
+        <span>1</span>
+        <input id="rating" type="range" min="1" max="5" v-model="rating" />
+        <span>5</span>
+      </div>
+      <div class="genre wrapper">
+        <label for="genre">Genre</label>
+        <select id="genre" name="genre" v-model="genre">
+          <option v-for="(genre, i) in genres" :key="i" :value="genre">{{
+            genre
+          }}</option>
+        </select>
+      </div>
+      <div class="desc wrapper">
+        <label for="desc">Description</label>
+        <textarea
+          id="description"
+          name="description"
+          cols="30"
+          rows="10"
+          v-model="desc"
+        ></textarea>
+      </div>
       <button>Add</button>
-    </div>
+    </form>
+    <button>Clear</button>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
+  data() {
     return {
+      title: "",
+      rating: "3",
+      genre: "",
+      desc: "",
       genres: [
         "Drama",
         "Action",
         "Comedy",
         "Fantasy",
         "Reality",
-        "Science Fiction"
-      ]
-    }
-  }
+        "Science Fiction",
+      ],
+    };
+  },
+
+  methods: {
+    createMovie() {
+      let newMovie = {
+        title: this.title,
+        rating: +this.rating,
+        genre: this.genre,
+        desc: this.desc
+      }
+
+      this.$emit("add-new-movie", newMovie)
+    },
+  },
 };
 </script>
 
 <style scoped>
-  .add-movie {
-    border: 2px solid black;
-    border-radius: 10px;
-    display: inline-block;
-    margin: 1rem 1rem;
-    padding: 2rem;
-  }
+.add-movie {
+  border: 2px solid black;
+  border-radius: 10px;
+  display: inline-block;
+  margin: 1rem 1rem;
+  padding: 2rem;
+}
 
-  input,
-  label {
-    margin: 0 1rem;
-  }
+input,
+label {
+  margin: 0 1rem;
+}
 
-  .desc label {
-    align-self: flex-start;
-  }
+.desc label {
+  align-self: flex-start;
+}
 
-  .wrapper {
-    align-items: center;
-    display: flex;
-    margin: 1rem 0;
-  }
+.wrapper {
+  align-items: center;
+  display: flex;
+  margin: 1rem 0;
+}
 
-  .button.wrapper {
-    justify-content: center;
-  }
+.button.wrapper {
+  justify-content: center;
+}
 
-  button {
-    margin: 0 2rem;
-  }
+button {
+  margin: 0 2rem;
+}
 </style>
