dc7ab826df6cf8106ad34f4d40a709b5ef1c2dd3
Niklas Håkansson <n.hakansson@hotmail.com>
2021-01-26 11:41:38 +0100

    Update for MovieList
    Queustion 7 done.

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,31 @@
 <template>
   <div id="app">
     <!-- <router-view /> -->
-    <AddMovie @add-new-movie="addMovieToMovies" />
-    <div v-if="movies.length > 0">
-      <MovieCard v-for="(movie, i) in movies" :key="i" :movie="movie" />
-    </div>
+    <AddMovie @add-new-movie="sendNewMovieToMovieList" />
+    <MovieList :newMovie="newMovie" />
   </div>
 </template>
 
 <script>
 import AddMovie from "./views/AddMovie";
-import MovieCard from "./views/MovieCard";
+import MovieList from "./views/MovieList";
 
 export default {
   components: {
     AddMovie,
-    MovieCard,
+    MovieList,
   },
 
-  data() {
+  data(){
     return {
-      movies: [
-        {
-          title: "Harry Potter",
-          rating: 4,
-          genre: "Fantasy",
-          desc: "Lorem Upsum",
-        },
-        {
-          title: "300",
-          rating: 5,
-          genre: "Fantasy",
-          desc: "Lorem Upsum with Sparta",
-        },
-      ],
-    };
+      newMovie: null
+    }
   },
 
   methods: {
-    addMovieToMovies(newMovie) {
-      this.movies.push(newMovie);
-      console.log("In App.vue - movies: ", this.movies);
-    },
+    sendNewMovieToMovieList(movie){
+      this.newMovie = movie;
+    }
   },
 };
 </script>
---
+++ src/views/MovieList.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="movie-list">
+    <div v-if="movies.length > 0">
+      <MovieCard v-for="(movie, i) in movies" :key="i" :movie="movie" />
+    </div>
+  </div>
+</template>
+
+<script>
+import MovieCard from "./MovieCard";
+
+export default {
+  components: {
+    MovieCard,
+  },
+
+  props: ["newMovie"],
+
+  watch: {
+    newMovie: function(newMovie, oldVal){
+      console.log("Old value: ", oldVal)
+      console.log("New value: ", newMovie)
+      this.movies.unshift(newMovie)
+    }
+  },
+
+  data() {
+    return {
+      movies: [
+        {
+          title: "Harry Potter",
+          rating: 4,
+          genre: "Fantasy",
+          desc: "Lorem Upsum",
+        },
+        {
+          title: "300",
+          rating: 5,
+          genre: "Fantasy",
+          desc: "Lorem Upsum with Sparta",
+        },
+      ],
+    };
+  },
+
+  methods: {
+    addMovieToMovies(newMovie) {
+      this.movies.push(newMovie);
+      console.log("In App.vue - movies: ", this.movies);
+    },
+  },
+};
+</script>
+
+<style></style>
